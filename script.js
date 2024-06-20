b1f3a726f8b357977e0494dbf40f8498c256f14c
Oksana <ksushatarasenko@gmail.com>
2024-06-20 07:10:50 +0200

    new

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleAnswer(number) {
+    var answer = document.getElementById('answer' + number);
+    if (answer.style.display === 'none' || answer.style.display === '') {
+        answer.style.display = 'block';
+    } else {
+        answer.style.display = 'none';
+    }
+}
